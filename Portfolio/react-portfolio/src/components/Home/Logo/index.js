51a06e49d81c7c59794925ad5ef70122d59e34d8
SaadHusayn <rsaad5095@gmail.com>
2024-08-07 12:52:52 +0500

    fixed gsap drawSVG freeze

--- Portfolio/react-portfolio/src/components/Home/Logo/index.js
+++ Portfolio/react-portfolio/src/components/Home/Logo/index.js
@@ -1,6 +1,6 @@
 import './index.scss'
 import LogoS from '../../../assets/images/logo-s.png'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import gsap from 'gsap-trial'
 import { DrawSVGPlugin } from 'gsap-trial/DrawSVGPlugin'
 
@@ -8,20 +8,30 @@ const Logo = () => {
   const bgRef = useRef()
   const outlineLogoRef = useRef()
   const solidLogoRef = useRef()
+  const didAnimate = useRef(false) // Add this useRef to track if animation has already run, we using useRef here instead of useState because useRef doesnot trigger a re-render when its value is modified unlike useState
+
 
   useEffect(() => {
+    if (didAnimate.current) return // Check if animation has run before, if yes, do nothing
+    didAnimate.current = true
+    
     gsap.registerPlugin(DrawSVGPlugin)
+
+    
 
     gsap.timeline()
     .to(bgRef.current, {
         duration:1,
         opacity: 1,
     })
-    // .from(outlineLogoRef.current, {
-    //     drawSVG:1,
-    //     duration:20,
+    .from(outlineLogoRef.current, {
+        drawSVG:0,
+        duration:20,
         
-    // })
+    })
+    
+    
+    
     
 
     gsap.fromTo(solidLogoRef.current, {

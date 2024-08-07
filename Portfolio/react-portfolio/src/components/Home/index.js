85746360da2c3bb611b8a2f326dd207493bdf811
SaadHusayn <rsaad5095@gmail.com>
2024-08-07 11:34:48 +0500

    done with home page for now

--- Portfolio/react-portfolio/src/components/Home/index.js
+++ Portfolio/react-portfolio/src/components/Home/index.js
@@ -1,29 +1,43 @@
 import { Link } from 'react-router-dom'
 import LogoTitle from '../../assets/images/logo-s.png'
 import './index.scss'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
+import Logo from './Logo'
 
 const Home = () => {
-    const [letterClass, setLetterClass] = useState('text-animate');
+  const [letterClass, setLetterClass] = useState('text-animate')
 
-
+  // useEffect hook is used to perform side effects i.e fetching data, timers, and modifying DOM explicitly
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+      console.log('hello')
+    }, 4000)
+  }, [])
 
   return (
     <div className="container home-page">
       <div className="text-zone">
         <h1>
-          Hi, <br /> <AnimatedLetters letterClass={letterClass} str={"I'm"} index={12} />
+          Hi, <br />{' '}
+          <AnimatedLetters letterClass={letterClass} str={"I'm"} index={12} />
           <img src={LogoTitle} alt="developer" />
-          <AnimatedLetters letterClass={letterClass} str={'aad'} index={15} />  
+          <AnimatedLetters letterClass={letterClass} str={'aad'} index={15} />
           <br />
-          <AnimatedLetters letterClass={letterClass} str={"web developer"} index={22} />
+          <AnimatedLetters
+            letterClass={letterClass}
+            str={'web developer'}
+            index={22}
+          />
         </h1>
         <h2>Full Stack Developer / Python Expert / CS Student</h2>
         <Link to="/contact" className="flat-button">
           CONTACT ME
         </Link>
       </div>
+
+      <Logo />
     </div>
   )
 }
